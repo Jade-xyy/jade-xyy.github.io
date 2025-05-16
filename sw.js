7ef5154fcb954d7553a30860a18cc8e8e315fa35
xuey35 <281108530@qq.com>
2025-05-16 08:34:00 +0800

    Site updated: 2025-05-16 08:33:56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747300085343";const preCache = ["/images/taichi.png","/images/zzz_cjjxrrdpts.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747355630266";const preCache = ["/images/taichi.png","/images/zzz_cjjxrrdpts.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
